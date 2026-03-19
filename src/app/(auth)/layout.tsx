7a778bee2066b7ea772cf884d4fcfd1c9743efab
Jared <jared0192galicia@gmail.com>
2026-03-19 10:35:13 -0600

    Add auth page

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -6,14 +6,7 @@ export default function AuthLayout({
   return (
     // Fondo oscuro profundo y centrado perfecto para el formulario
     <div className="min-h-screen bg-app-blue-900 flex items-center justify-center p-4">
-      <div className="w-full max-w-md">
-        {/* Aquí podrías poner un logo pequeño que aparezca arriba de todos los formularios de auth */}
-        <div className="text-center mb-8">
-          <h1 className="text-app-white text-3xl font-bold tracking-tight">
-            SIRCAR
-          </h1>
-        </div>
-
+      <div className="">
         {children}
       </div>
     </div>
--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,54 +1,66 @@
-'use client';
+"use client";
 import { useState } from "react";
-import { Mail, Lock, Eye, EyeOff } from "lucide-react";
+import { Mail, Lock, Eye, EyeOff, LogIn } from "lucide-react";
+import IconLogo from "@/shared/ui/WhiteLogo";
+import Button from "@/shared/ui/ButtonPrimary";
 
 export default function LoginForm() {
   const [showPassword, setShowPassword] = useState(false);
 
   return (
-    <div className="flex flex-col gap-6 w-full max-w-md p-8 bg-[#020817]">
-      {/* Input de Usuario/Correo */}
-      <div className="relative group">
-        <div className="absolute inset-y-0 left-5 flex items-center pointer-events-none">
-          <Mail
-            className="text-[#94A3B6] group-focus-within:text-[#3B82F6] transition-colors"
-            size={24}
-            strokeWidth={1.5}
+    <>
+      <div className="w-full flex justify-center">
+        <IconLogo />
+      </div>
+      <div className="flex p-20 px-32 flex-col gap-6 w-full md:w-[600px] m-5 bg-app-blue-800 rounded-xl">
+        {/* Input de Usuario/Correo */}
+        <div className="relative group">
+          <div className="absolute inset-y-0 left-5 flex items-center pointer-events-none">
+            <Mail
+              className="text-app-gray-500 group-focus-within:text-app-blue-400 transition-colors"
+              size={24}
+              strokeWidth={1.5}
+            />
+          </div>
+          <input
+            type="text"
+            placeholder="Usuario o Correo"
+            className="w-full h-14 pl-14 pr-6 bg-transparent border-2 border-app-gray-200/20 rounded-2xl text-app-white placeholder-app-gray-500 outline-none focus:border-app-blue-400 transition-all text-lg"
           />
         </div>
-        <input
-          type="text"
-          placeholder="Usuario o Correo"
-          className="w-full h-16 pl-14 pr-6 bg-transparent border-2 border-[#DAE8F4]/20 rounded-3xl text-[#F8FAFC] placeholder-[#94A3B6] outline-none focus:border-[#3B82F6] transition-all text-lg"
-        />
-      </div>
 
-      {/* Input de Contraseña */}
-      <div className="relative group">
-        <div className="absolute inset-y-0 left-5 flex items-center pointer-events-none">
-          <Lock
-            className="text-[#94A3B6] group-focus-within:text-[#3B82F6] transition-colors"
-            size={24}
-            strokeWidth={1.5}
+        {/* Input de Contraseña */}
+        <div className="relative group">
+          <div className="absolute inset-y-0 left-5 flex items-center pointer-events-none">
+            <Lock
+              className="text-app-gray-500 group-focus-within:text-app-blue-400 transition-colors"
+              size={24}
+              strokeWidth={1.5}
+            />
+          </div>
+          <input
+            type={showPassword ? "text" : "password"}
+            placeholder="Contraseña"
+            className="w-full h-14 pl-14 pr-14 bg-transparent border-2 border-app-gray-200/20 rounded-2xl text-app-white placeholder-app-gray-500 outline-none focus:border-app-blue-400 transition-all text-lg"
           />
+          <button
+            type="button"
+            onClick={() => setShowPassword(!showPassword)}
+            className="absolute inset-y-0 right-5 flex items-center text-app-gray-500 hover:text-app-white transition-colors"
+          >
+            {showPassword ? (
+              <EyeOff size={24} strokeWidth={1.5} />
+            ) : (
+              <Eye size={24} strokeWidth={1.5} />
+            )}
+          </button>
         </div>
-        <input
-          type={showPassword ? "text" : "password"}
-          placeholder="Contraseña"
-          className="w-full h-16 pl-14 pr-14 bg-transparent border-2 border-[#DAE8F4]/20 rounded-3xl text-[#F8FAFC] placeholder-[#94A3B6] outline-none focus:border-[#3B82F6] transition-all text-lg"
-        />
-        <button
-          type="button"
-          onClick={() => setShowPassword(!showPassword)}
-          className="absolute inset-y-0 right-5 flex items-center text-[#94A3B6] hover:text-[#F8FAFC] transition-colors"
-        >
-          {showPassword ? (
-            <EyeOff size={24} strokeWidth={1.5} />
-          ) : (
-            <Eye size={24} strokeWidth={1.5} />
-          )}
-        </button>
+        <span className="text-sm text-app-gray-500 text-right w-full">
+          ¿Olvidaste tu contraseña?
+        </span>
+
+        <Button label="Ingresar" className="my-6 w-full m-auto border-app-white/60 border" icon={<LogIn />}></Button>
       </div>
-    </div>
+    </>
   );
 }
